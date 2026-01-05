1d8015684a2706b20c08f32886a34559c3000e37
Mohamed Hamada <mohamedhamada@gmail.com>
2026-01-05 17:00:11 +0200

    trigger cubit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
--- lib/widget/add_note_form.dart
+++ lib/widget/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:folder/cubits/add_note_cuit/add_note_cubit.dart';
+import 'package:folder/models/note_model.dart';
 import 'package:folder/widget/custom_button.dart';
 import 'package:folder/widget/custom_text_form_field.dart';
 
@@ -42,6 +45,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                var noteModel = NoteModel(
+                  title: title!,
+                  subtitle: content!,
+                  date: DateTime.now().toString(),
+                  color: Colors.blue.value,
+                );
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autoValidateMode = AutovalidateMode.always;
                 setState(() {});
