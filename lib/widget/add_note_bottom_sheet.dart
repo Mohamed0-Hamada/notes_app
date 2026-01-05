a3ad971b5426ba8d6ad2b0982cab88c4d0486679
Mohamed Hamada <mohamedhamada@gmail.com>
2026-01-05 16:17:18 +0200

    add note add add note cubit to add note  bottom sheet

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:folder/cubits/add_note_cuit/add_note_cubit.dart';
 import 'package:folder/widget/add_note_form.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(child: AddNoteForm()),
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+   if(state is AddNoteSuccess){
+        Navigator.pop(context);
+   }else if(state is AddNoteFailure){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(state.errorMessage),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+   }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SingleChildScrollView(
+            child: ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: AddNoteForm(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
