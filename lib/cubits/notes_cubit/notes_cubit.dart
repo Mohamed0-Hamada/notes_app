327e882d000855588daadc358f0fb311b367f378
Mohamed Hamada <mohamedhamada@gmail.com>
2026-01-05 19:37:25 +0200

    display notes

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -2,21 +2,14 @@ import 'package:bloc/bloc.dart';
 import 'package:folder/constants.dart';
 import 'package:folder/models/note_model.dart';
 import 'package:hive/hive.dart';
-import 'package:meta/meta.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
+  List<NoteModel>? notes;
   fetchAllNotes() async {
-    try {
-      emit(NotesLoading());
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/widget/note_view_body.dart
+++ lib/widget/note_view_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:folder/cubits/notes_cubit/notes_cubit.dart';
 import 'package:folder/widget/custom_app_bar.dart';
 
 import 'package:folder/widget/notes_list_view.dart';
 
-class NoteViewBody extends StatelessWidget {
+class NoteViewBody extends StatefulWidget {
   const NoteViewBody({super.key});
+
+  @override
+  State<NoteViewBody> createState() => _NoteViewBodyState();
+}
+
+class _NoteViewBodyState extends State<NoteViewBody> {
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +25,9 @@ class NoteViewBody extends StatelessWidget {
       child: Column(
         children: const [
           SizedBox(height: 50),
-          CustomAppBar(icon: Icons.search, title: 'Notes',),
+          CustomAppBar(icon: Icons.search, title: 'Notes'),
           SizedBox(height: 16),
           Expanded(child: NotesListView()),
-          
         ],
       ),
     );
